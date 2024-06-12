a558f3bc2e801d8454e744066510858f2676684c
ISreapeRI <maxim.iurivitch2014@yandex.ru>
2024-06-13 01:41:17 +0300

    added project files

---
+++ Assets/Scripts/ScoreHandler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject.SpaceFighter;
+
+public class ScoreHandler
+{
+    public int? playerScore { get; private set; }
+
+    public ScoreHandler()
+    {
+        if (PlayerPrefs.HasKey("Best score"))
+        {
+            playerScore = PlayerPrefs.GetInt("Base score");
+        }
+        else
+        {
+            playerScore = null;
+        } 
+    }
+}
---
+++ Assets/Scripts/UI/GameStart.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+    // Начинаем игру
+    public void StartGame()
+    {
+        Debug.Log("Игра начинается!");
+    }
+}
